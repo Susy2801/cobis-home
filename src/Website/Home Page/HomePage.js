8206a51d34cf93c5f82c3146733ea037ec59756e
Susy2801 <nduy200@gmail.com>
2024-03-13 11:30:14 +0700

    finish hero part for homepage

--- src/Website/Home Page/HomePage.js
+++ src/Website/Home Page/HomePage.js
@@ -1,6 +1,21 @@
 import "./HomePage.css";
+import heroImg from "../Asset/Rectangle 5.png";
 function HomePage() {
-  return <div className="home__container"></div>;
+  return (
+    <div className="home__container">
+      <div className="home__hero">
+        <div className="home__hero--left">
+          <h3>Cobi's Home</h3>
+          <h2>Hotel for every moment rich in emotion</h2>
+          <h5>Every moment feels like the first time in paradise view</h5>
+          <button>Book now</button>
+        </div>
+        <div className="home__hero--right">
+          <img src={heroImg} alt="hero-img" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
